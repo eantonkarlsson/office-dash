385f5a383d83a15be6b0122f9c3d55cf5a91c8d0
eantonkarlsson <e.anton.karlsson@gmail.com>
2017-07-18 16:30:32 +0200

    Update twitter_widget.rb

--- twitter_widget.rb
+++ twitter_widget.rb
@@ -4,10 +4,10 @@ require 'twitter'
 #### Get your twitter keys & secrets:
 #### https://dev.twitter.com/docs/auth/tokens-devtwittercom
 twitter = Twitter::REST::Client.new do |config|
-  config.consumer_key = 'YOUR_CONSUMER_KEY'
-  config.consumer_secret = 'YOUR_CONSUMER_SECRET_KEY'
-  config.access_token = 'YOUR_ACCESS_TOKEN'
-  config.access_token_secret = 'YOUR_ACCESS_TOKEN_SECRET'
+  config.consumer_key = '5TmkfvxK6SvsLVVPyAxCFvm3T'
+  config.consumer_secret = 'TMIDE6gFhXMHmj8vL4yW2TuEz7ZMihDWuxJb9SRNZjBoRzX7Ry'
+  config.access_token = '	887316246743666688-BLxGxhTQJ3KDilWn2HvZ3iuWW92E51Y'
+  config.access_token_secret = 'umb5pxE4WCtG2HP6V5y8vhuUVITWdxO5QZHM3CKvMRBVF'
 end
 
 SCHEDULER.every '15m', :first_in => 0 do |job|
